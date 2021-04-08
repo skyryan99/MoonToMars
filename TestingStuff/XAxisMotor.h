94ba299aac386ff435f4040d7726ea7ec847668f
subpar-coder <77028155+subpar-coder@users.noreply.github.com>
2021-04-08 14:41:40 -0700

    Add files via upload

--- TestingStuff/XAxisMotor.h
+++ TestingStuff/XAxisMotor.h
@@ -15,14 +15,6 @@
 #define ENA_PIN P5
 #define ENA_BIT BIT1
 
-#define pin2out (0x40004c03)
-#define pin2dir (0x40004c05)
-#define pin1in (0x40004c00)
-#define pin1out (0x40004c02)
-#define pin1dir (0x40004c04)
-#define pin5dir (0x40004c32)
-#define pin5out (0x40004c34)
-
 void MoveLeft(uint32_t runtime)
 void MoveRight(uint32_t runtime)
 
