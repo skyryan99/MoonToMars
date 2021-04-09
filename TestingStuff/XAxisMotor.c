a90caa9fabdcb9cbc2690d67ec3dd5136f294726
skyryan99 <49254131+skyryan99@users.noreply.github.com>
2021-04-09 10:36:29 -0700

    Update XAxisMotor.c

--- TestingStuff/XAxisMotor.c
+++ TestingStuff/XAxisMotor.c
@@ -5,13 +5,6 @@
 
 void initStepperMotors()
 {
-    ;
-}
-
-//runtime is in milliseconds
-void MoveLeft(uint32_t runtime)
-{
-    int x;
     DIR_PIN->SEL1 &= ~(DIR_BIT);                    /* Use GPIO */
     DIR_PIN->SEL0 &= ~(DIR_BIT);
     DIR_PIN->DIR |= DIR_BIT;
@@ -22,6 +15,12 @@ void MoveLeft(uint32_t runtime)
     ENA_PIN->SEL0 &= ~(ENA_BIT);
     ENA_PIN->DIR |= ENA_BIT;
     ENA_PIN->OUT &= ~(ENA_BIT)
+}
+
+//runtime is in milliseconds
+void MoveLeft(uint32_t runtime)
+{
+    uint32_t x;
 
     DIR_PIN->OUT &= ~(DIR_BIT);
     for (x = 0; x<runtime; x++)
@@ -43,18 +42,7 @@ void MoveLeft(uint32_t runtime)
 
 void MoveRight(uint32_t runtime)
 {
-    int x;
-
-    DIR_PIN->SEL1 &= ~(DIR_BIT);                    /* Use GPIO */
-    DIR_PIN->SEL0 &= ~(DIR_BIT);
-    DIR_PIN->DIR |= DIR_BIT;
-    PUL_PIN->SEL1 &= ~(PUL_BIT);
-    PUL_PIN->SEL0 &= ~(PUL_BIT);
-    PUL_PIN->DIR |= PUL_BIT;
-    ENA_PIN->SEL1 &= ~(ENA_BIT);
-    ENA_PIN->SEL0 &= ~(ENA_BIT);
-    ENA_PIN->DIR |= ENA_BIT;
-    ENA_PIN->OUT &= ~(ENA_BIT)
+    uint32_t x;
 
     DIR_PIN->OUT |= DIR_BIT;
         for (x = 0; x<runtime; x++)
