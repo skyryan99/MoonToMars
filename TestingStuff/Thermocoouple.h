2a94a16a84015469f0aa179af4d811eee9eb1c65
rrodri98 <77022860+rrodri98@users.noreply.github.com>
2021-05-22 11:40:08 -0700

    Create Thermocoouple.h

---
+++ TestingStuff/Thermocoouple.h
@@ -0,0 +1,16 @@
+/*
+ * Thermocouple.h
+ *
+ *  Created on: May 22, 2021
+ *      Author: Rebecca Rodriguez
+ */
+
+#ifndef THERMOCOUPLE_H_
+#define THERMOCOUPLE_H_
+
+
+#define CS_PIN P2              /*Chip-Select (Slave-Select) may be any GPIO pin*/
+#define CS_BIT BIT3
+#define CAL_CONST 0.25
+
+#endif /* THERMOCOUPLE_H_ */
